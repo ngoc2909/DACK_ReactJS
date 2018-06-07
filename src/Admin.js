0aaecbad266237a4b0d9bcb46f6f852839a259fd
DELL-PC\DELL <myngoc3456@gmail.com>
2018-06-07 10:37:03 +0700

    Phần Admin Quản lý sản phẩm

---
+++ src/Admin.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import QuanLySanPham from './components/QuanLySanPham';
+
+class Admin extends Component {
+
+
+    render() {
+        return (
+            <Router>
+                <div>
+                    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+                        <div className="navbar-brand"><h1> Menu </h1></div>
+                        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                                data-target="#navbarResponsive" aria-controls="navbarResponsive"
+                                aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarResponsive">
+                            <ul className="navbar-nav navbar-sidenav" id="exampleAccordion">
+                                <li className="nav-item" data-toggle="tooltip" data-placement="right"
+                                    title="Dashboard">
+                                    <div className="nav-link">
+                                        <i className="fa fa-fw fa-table"></i>
+                                        <span className="nav-link-text"><Link to={'/QuanLySanPham'}>Quản lý sản phẩm</Link></span>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            <Switch>
+            <Route exact path='/QuanLySanPham' component={QuanLySanPham} />
+            </Switch>
+            </Router>
+        );
+
+    }
+}
+export default Admin;
---
+++ src/components/QuanLySanPham.js
@@ -0,0 +1,136 @@
+import React, { Component } from 'react';
+import { Button, Collapse } from 'react-bootstrap'
+import Modal from 'react-modal';
+import '../App.css';
+import ModalAdd from '../components/ModalAdd';
+
+class QuanLySanPham extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:3002/api/sanpham")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        items: result
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+        openModal = () => {
+            this.setState({modalIsOpen: true});
+        }
+        closeModal = () => {
+            this.setState({modalIsOpen: false});
+        }
+
+
+render() {
+        const {error, isLoaded, items} = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <div>
+                    <div className="content-wrapper">
+                        <div className="container-fluid">
+                            <ol className="breadcrumb">
+                                <li className="breadcrumb-item">
+                                    <a href="#">Dashboard</a>
+                                </li>
+                                <li className="breadcrumb-item active">Tables</li>
+                            </ol>
+                            <div className="album py-5 bg-light">
+                                <div className="container">
+                        <h2>Quản Lý Sản Phẩm</h2>
+                                    <p><button onClick={this.openModal}>Add new Product</button></p>
+                     <p>   {items.map(item => (
+                            <div className="card-body">
+                                <Modal show={this.state.show} onHide={this.handleClose}
+                                    isOpen={this.state.modalIsOpen}
+                                    onRequestClose={this.closeModal}
+                                    contentLabel="Example Modal"
+                                >
+                                    <button onClick={this.closeModal}>Close</button>
+                                    <ModalAdd/>
+                                </Modal>
+                                <div className="table-responsive">
+                                    <table className="table table-bordered" id="dataTable" width="100%" cellSpacing="1"
+                                           border="1">
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Image-url</th>
+                                            <th>Tên Sản Phẩm</th>
+                                            <th>Mô tả</th>
+                                            <th>Chức năng</th>
+
+                                        </tr>
+                                        <tbody>
+                                        <td>{item.masp}</td>
+                                        <td>
+                                            <div><img className="card-img-top"
+                                                      src={item.image_url}/>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <p>{item.name}</p>
+                                        </td>
+                                        <td>
+                                            <p>{item.description}</p>
+                                        </td>
+                                        <td>
+                                            <div className="btn-group">
+                                                <Button onClick={this.handleView}>View</Button>
+                                                <Button onClick={this.props.handleEdit}>Edit</Button>
+                                                <Button onClick={() => this.props.handleDelete(this.props.name)}>Delete</Button>
+                                            </div>
+                                            <footer className="sticky-footer">
+                                                <div className="container">
+                                                    <div className="text-center">
+                                                        <small>Copyright © Your Website 2018</small>
+                                                    </div>
+                                                </div>
+                                            </footer>
+                                            <a className="scroll-to-top rounded" href="#page-top">
+                                                <i className="fa fa-angle-up"></i>
+                                            </a>
+                                        </td>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+
+
+                        ))
+                     }</p>
+                    </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+    }
+}
+export default QuanLySanPham;
